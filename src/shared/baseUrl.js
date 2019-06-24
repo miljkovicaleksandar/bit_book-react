b46e102a37c75be37bbf0c0a9b634ca4a2ca1489
BIT Student <classroom@bgit.rs>
2019-06-24 11:33:14 +0200

    komentar

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://book-api.hypetech.xyz/v1";
-// const BASE_URL = "http://book-api-dev.hypetech.xyz";
+// const BASE_URL = "https://book-api.hypetech.xyz/v1"; // Na produkcionom : Radi login i register(vrati info o uspesnom registeru ali nije moguce da se loguje sa tim korisnikom), postovanje i komentarisanje, 
+const BASE_URL = "http://book-api-dev.hypetech.xyz";    //Ne radi login (mora se ulogovati preko produkcijskog na dev acc), radi postavljanje i brisanje komentara , ne radi postavljanje postova
 export default BASE_URL;
